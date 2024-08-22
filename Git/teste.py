474eb5d348d065a79a9f9d49c9a49045de6a4746
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2024-08-21 22:37:51 -0300

    feat(project) Deleting teste.py
    It was deleted the python
    file used to test a script.

--- Git/teste.py
+++
@@ -1,6 +0,0 @@
-# Creation Date: 2024-08-21
-# Authors: Álvaro Ribeiro, Matheus Fonseca
-# Copyright 2024.
-# All rights are reserved. Reproduction in whole or part is prohibited without the written consent of the copyright owner.
-
-print("xpto")
