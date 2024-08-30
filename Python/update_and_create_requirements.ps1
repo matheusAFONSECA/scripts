e39611cd78988c71e971b9073a2c6ccbeebdd1d5
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2024-08-29 22:16:34 -0300

    feat(project): scripts to create a requirements
    It was created a script to run
    the commands necessary to create
    or atualize the requirements. It's
    necessary that the venv is activate.

---
+++ Python/update_and_create_requirements.ps1
@@ -0,0 +1,8 @@
+# Verifica se estamos em um ambiente virtual
+if (Test-Path env:VIRTUAL_ENV) {
+    # Atualiza o arquivo requirements.txt com os pacotes instalados
+    pip freeze > requirements.txt
+    Write-Host "requirements.txt updated successfully."
+} else {
+    Write-Host "Not working in a virtual environment, skipping requirements update."
+}
---
+++ Python/update_and_create_requirements.sh
@@ -0,0 +1,7 @@
+# update requirements file
+if [[ "$VIRTUAL_ENV" != "" ]]
+then
+  pip3 freeze > requirements.txt 
+else
+  echo "Not working in virtual env, skipping requirements update" 
+fi
